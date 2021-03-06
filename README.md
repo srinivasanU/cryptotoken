44084d8555eab09e285bbe97dc1dafce2ebc6107
srinivasan <1694srinivasan@gmail.com>
2021-03-06 21:05:03 +0530

    rest of the crypts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # What is this?
-
+This Encryption and Decryption is done using Cryptojs Library
 Get your data Encrypted/Decrypted for your app
 
 # Installation
@@ -9,15 +9,35 @@ Get your data Encrypted/Decrypted for your app
 Then...
 
 ```
-import { tokenEncrypterAES , tokenDecrypterAES } from 'tokencrypto';
+import { tokenEncrypterAES , tokenDecrypterAES , tokenEncrypterDES , tokenDecrypterDES , tokenEncrypterTripleDES , tokenDecrypterTripleDES , tokenEncrypterRabbit , tokenDecrypterRabbit , tokenEncrypterRC4 , tokenDecrypterRC4 , tokenEncrypterRC4Drop , tokenDecrypterRC4Drop } from 'tokencrypto';
 
 tokenEncrypterAES(Data , Secret key)
 tokenDecrypterAES(Encrypted Data,Secret key used while encrypting)
+tokenEncrypterDES(Data , Secret key)
+tokenDecrypterDES(Encrypted Data,Secret key used while encrypting)
+tokenEncrypterTripleDES(Data , Secret key)
+tokenDecrypterTripleDES(Encrypted Data,Secret key used while encrypting)
+tokenEncrypterRabbit(Data , Secret key)
+tokenDecrypterRabbit(Encrypted Data,Secret key used while encrypting)
+tokenEncrypterRC4(Data , Secret key)
+tokenDecrypterRC4(Encrypted Data,Secret key used while encrypting)
+tokenEncrypterRC4Drop(Data , Secret key)
+tokenDecrypterRC4Drop(Encrypted Data,Secret key used while encrypting)
 ```
 
 # Options
 
-Date Convertor supports these options:
+Encryption and Decryption have these options:
 
 * *tokenEncrypterAES* - _('hello','SECRET_KEY')_ 
 * *tokenDecrypterAES* - _('Encrypted_data','SECRET_KEY')_
+* *tokenEncrypterDES* - _('hello','SECRET_KEY')_ 
+* *tokenDecrypterDES* - _('Encrypted_data','SECRET_KEY')_
+* *tokenEncrypterTripleDES* - _('hello','SECRET_KEY')_ 
+* *tokenDecrypterTripleDES* - _('Encrypted_data','SECRET_KEY')_
+* *tokenEncrypterRabbit* - _('hello','SECRET_KEY')_ 
+* *tokenDecrypterRabbit* - _('Encrypted_data','SECRET_KEY')_
+* *tokenEncrypterRC4* - _('hello','SECRET_KEY')_ 
+* *tokenDecrypterRC4* - _('Encrypted_data','SECRET_KEY')_
+* *tokenEncrypterRC4Drop* - _('hello','SECRET_KEY')_ 
+* *tokenDecrypterRC4Drop* - _('Encrypted_data','SECRET_KEY')_
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var CryptoJS = require("crypto-js/core");
 CryptoJS.AES = require("crypto-js/aes");
+CryptoJS.DES = require("crypto-js/tripledes");
+CryptoJS.TripleDES = require("crypto-js/tripledes");
+CryptoJS.Rabbit = require("crypto-js/rabbit");
+CryptoJS.RC4 = require("crypto-js/rc4");
+CryptoJS.RC4Drop = require("crypto-js/rc4");
 
 module.exports.tokenEncrypterAES = (token,secretkey) => {
     var encrypted = CryptoJS.AES.encrypt(token, secretkey);
@@ -8,5 +13,55 @@ module.exports.tokenEncrypterAES = (token,secretkey) => {
 
 module.exports.tokenDecrypterAES = (token,secretkey) => {
     var decrypted = CryptoJS.AES.decrypt(token, secretkey);
+    return decrypted.toString(CryptoJS.enc.Utf8)
+}
+
+module.exports.tokenEncrypterDES = (token,secretkey) => {
+    var encrypted = CryptoJS.DES.encrypt(token, secretkey);
+    return encrypted.toString()
+}
+
+module.exports.tokenDecrypterDES = (token,secretkey) => {
+    var decrypted = CryptoJS.DES.decrypt(token, secretkey);
+    return decrypted.toString(CryptoJS.enc.Utf8)
+}
+
+module.exports.tokenEncrypterTripleDES = (token,secretkey) => {
+    var encrypted = CryptoJS.TripleDES.encrypt(token, secretkey);
+    return encrypted.toString()
+}
+
+module.exports.tokenDecrypterTripleDES = (token,secretkey) => {
+    var decrypted = CryptoJS.TripleDES.decrypt(token, secretkey);
+    return decrypted.toString(CryptoJS.enc.Utf8)
+}
+
+module.exports.tokenEncrypterRabbit = (token,secretkey) => {
+    var encrypted = CryptoJS.Rabbit.encrypt(token, secretkey);
+    return encrypted.toString()
+}
+
+module.exports.tokenDecrypterRabbit = (token,secretkey) => {
+    var decrypted = CryptoJS.Rabbit.decrypt(token, secretkey);
+    return decrypted.toString(CryptoJS.enc.Utf8)
+}
+
+module.exports.tokenEncrypterRC4 = (token,secretkey) => {
+    var encrypted = CryptoJS.RC4.encrypt(token, secretkey);
+    return encrypted.toString()
+}
+
+module.exports.tokenDecrypterRC4 = (token,secretkey) => {
+    var decrypted = CryptoJS.RC4.decrypt(token, secretkey);
+    return decrypted.toString(CryptoJS.enc.Utf8)
+}
+
+module.exports.tokenEncrypterRC4Drop = (token,secretkey) => {
+    var encrypted = CryptoJS.RC4Drop.encrypt(token, secretkey);
+    return encrypted.toString()
+}
+
+module.exports.tokenDecrypterRC4Drop = (token,secretkey) => {
+    var decrypted = CryptoJS.RC4Drop.decrypt(token, secretkey);
     return decrypted.toString(CryptoJS.enc.Utf8)
 }
